82a05d668b33f53023a4961b20f054347ccec415
gmarte <giancarlo.marte@gmail.com>
2022-07-19 10:40:08 -0400

    set up home page and entry pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,10 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def entry(request, entry):
+    return render(request, "encyclopedia/entry.html", {
+        "title": entry,
+        "entry": util.get_entry(entry)
+    })
