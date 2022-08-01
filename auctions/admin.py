bf18f97f2259d2bf9f5560a49efc4b76d66297c8
gmarte <giancarlo.marte@gmail.com>
2022-08-01 18:56:45 -0400

    Some models functionalities

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Auction, Comment, Category, Bid, Watchlist
+
+admin.site.register(Auction)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Bid)
+admin.site.register(Watchlist)
--- auctions/models.py
+++ auctions/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -5,3 +6,34 @@ from django.db import models
 class User(AbstractUser):
     def __str__(self):
         return f"{self.username}"
+class Category(models.Model):
+    text = models.CharField(max_length=64)        
+class Auction(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    start_bid = models.FloatField()        
+    categories = models.ManyToManyField(Category)
+    urlimg = models.CharField(max_length=255)
+    date = models.DateField(auto_now_add=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+    def __str__(self):
+        return f"{self.title} started at {self.start_bid} now we are here "
+class Comment(models.Model):
+    text = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)   
+    def __str__(self):
+        return f"{self.text} by {self.user} on {self.date}"
+
+class Bid(models.Model):
+    price = models.FloatField()
+    date = models.DateField(auto_now_add=True)
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)    
+    user = models.ForeignKey(User, on_delete=models.PROTECT)   
+    def __str__(self):
+        return f"{self.price} ({self.user})"
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction = models.ManyToManyField(Auction, related_name='watchlist', blank=True)
