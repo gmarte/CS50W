72b5518ede9d8a10e625cb52f19df8185595756a
gmarte <giancarlo.marte@gmail.com>
2022-08-01 22:40:41 -0400

    Models nav etc

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Auction, Comment, Category, Bid, Watchlist
+from .models import Auction, Comment, Category, Bid, Watchlist, User
 
 admin.site.register(Auction)
 admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(Bid)
 admin.site.register(Watchlist)
+admin.site.register(User)
--- auctions/models.py
+++ auctions/models.py
@@ -6,23 +6,28 @@ from django.db import models
 class User(AbstractUser):
     def __str__(self):
         return f"{self.username}"
+#categories
 class Category(models.Model):
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
     text = models.CharField(max_length=64)        
 class Auction(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField()
     start_bid = models.FloatField()        
-    categories = models.ManyToManyField(Category)
-    urlimg = models.CharField(max_length=255)
+    categories = models.ManyToManyField(Category, related_name="categories")
+    urlimg = models.CharField(max_length=255,blank=True)
     date = models.DateField(auto_now_add=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.BooleanField(default=True)
     def __str__(self):
-        return f"{self.title} started at {self.start_bid} now we are here "
+        return f"{self.title}"
+# auctions comments
 class Comment(models.Model):
     text = models.TextField()
     date = models.DateField(auto_now_add=True)
-    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)    
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comments")    
     user = models.ForeignKey(User, on_delete=models.CASCADE)   
     def __str__(self):
         return f"{self.text} by {self.user} on {self.date}"
@@ -30,10 +35,10 @@ class Comment(models.Model):
 class Bid(models.Model):
     price = models.FloatField()
     date = models.DateField(auto_now_add=True)
-    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)    
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="bids")    
     user = models.ForeignKey(User, on_delete=models.PROTECT)   
     def __str__(self):
         return f"{self.price} ({self.user})"
 class Watchlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    auction = models.ManyToManyField(Auction, related_name='watchlist', blank=True)
+    auction = models.ManyToManyField(Auction, related_name="watchlist", blank=True)
