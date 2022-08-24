4593430aad1d6348d7ba17d2ffde6262d7c32c85
gmarte <giancarlo.marte@gmail.com>
2022-08-23 23:42:12 -0400

    send mail and list of emails view

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,14 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-  document.querySelector('#sendmail').addEventListener('click', sendmail);
+  
+  // send mail
+  document.querySelector('#compose-form').onsubmit = sendmail;
 
   // By default, load the inbox
   load_mailbox('inbox');
 });
+
+// send email function
 function sendmail(event){
   event.preventDefault()
-
   const recipients = document.querySelector("#compose-recipients").value;  
   const subject = document.querySelector("#compose-subject").value;  
   const body = document.querySelector("#compose-body").value;
@@ -25,12 +28,22 @@ function sendmail(event){
       body: body
     })
   })
-  .then(response => response.json())
+  .then(response => { 
+    if (!response.ok) {throw response }
+    else{return response.json()}
+  })
   .then(result => {
-    console.log(result)
+    console.log(result);    // console log the result
+  })
+  .catch((error) => { // error handling
+    error.text().then( errorMessage => {
+      console.error(errorMessage);     // log the error  
+      document.querySelector("#errorMessage").classList.remove('d-none');          
+      document.querySelector("#errorMessage").classList.add('d-block');  
+      document.querySelector("#errorMessage").innerHTML = errorMessage;
+    })              
   });
-  //load_mailbox('sent');  
-  return false;
+  load_mailbox('sent');
 }
 
 function compose_email() {
@@ -52,5 +65,6 @@ function load_mailbox(mailbox) {
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  // document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+
