4a68ce68336792f96bdc9aaec5ee23a7a3078c2c
gmarte <giancarlo.marte@gmail.com>
2022-08-29 17:41:22 -0400

    read/archive/reply feat

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,3 +1,25 @@
+const unReadIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-open-fill" viewBox="0 0 16 16">
+<path d="M8.941.435a2 2 0 0 0-1.882 0l-6 3.2A2 2 0 0 0 0 5.4v.314l6.709 3.932L8 8.928l1.291.718L16 5.714V5.4a2 2 0 0 0-1.059-1.765l-6-3.2ZM16 6.873l-5.693 3.337L16 13.372v-6.5Zm-.059 7.611L8 10.072.059 14.484A2 2 0 0 0 2 16h12a2 2 0 0 0 1.941-1.516ZM0 13.373l5.693-3.163L0 6.873v6.5Z"/>
+</svg>
+<span class="visually-hidden">Unread</span>`;
+const readIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+<path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+</svg>
+<span class="visually-hidden">Read</span>`;
+const replyIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply" viewBox="0 0 16 16">
+<path d="M6.598 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.74 8.74 0 0 0-1.921-.306 7.404 7.404 0 0 0-.798.008h-.013l-.005.001h-.001L7.3 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L2.614 8.254a.503.503 0 0 0-.042-.028.147.147 0 0 1 0-.252.499.499 0 0 0 .042-.028l3.984-2.933zM7.8 10.386c.068 0 .143.003.223.006.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96v-.667z"/>
+</svg>
+  <span class="visually-hidden">Reply</span>`;
+const archiveIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive" viewBox="0 0 16 16">
+<path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"></path>
+</svg>
+<span class="visually-hidden">Archive</span>`;  
+
+const unArchiveIcon =`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive-fill" viewBox="0 0 16 16">
+<path d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
+</svg>
+<span class="visually-hidden">Back to inbox</span>`;
+
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
@@ -58,6 +80,47 @@ function compose_email() {
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';  
 }
+// function buttongroup(email){
+//   let div = document.createElement('div');
+//   div.className = 'btn-group mt-2';
+//    // create reply button & append to DOMContentLoaded
+//    const reply = document.createElement('button');
+//    reply.className = "btn btn-outline-primary";
+//    reply.innerHTML = replyIcon;
+//    reply.addEventListener('click', function() {
+//      compose_email();
+
+//      // populate fields with information from email
+//      document.querySelector('#compose-recipients').value = email['sender'];
+//      let subject = email['subject'];
+//      console.log(subject.split(" ", 1)[0]);
+//      if (subject.split(" ", 1)[0] != "Re:") {
+//        subject = "Re: " + subject;
+//      }
+//      document.querySelector('#compose-subject').value = subject;
+
+//      let body = `
+//        On ${email['timestamp']}, ${email['sender']} wrote: ${email['body']}
+//      `;
+//      document.querySelector('#compose-body').value = body;
+//    });
+//    div.appendChild(reply);
+
+
+//    const grparchive = document.createElement('button');
+//    grparchive.className = "btn btn-outline-primary";
+//    grparchive.innerHTML = archiveIcon;
+//    grparchive.addEventListener('click', email_archived(email['id'], !email['archived']));
+//    div.appendChild(grparchive);
+
+//    const grpread = document.createElement('button');
+//    grpread.className = "btn btn-outline-primary";
+//    grpread.innerHTML = readIcon;
+//    grpread.addEventListener('click', email_read(email['id'], !email['read']));
+//    div.appendChild(grpread);
+
+//    return div;
+// }
 function view_email(id){
   fetch('/emails/'+ id)
     .then(response => response.json())
@@ -65,32 +128,70 @@ function view_email(id){
       const email_views = document.querySelector('#emails-view');
       // Print email
       console.log(email);
+      // mark as read
+      email_read(id, true);         
       document.querySelector('#emails-view').style.display = 'none';
       document.querySelector('#compose-view').style.display = 'none';
       email_views.style.display = 'block';
-      // Show the mailbox name
-      email_views.innerHTML = `
-      <h3>${email['subject'].toUpperCase()}</h3>
-      <div class="btn-group">
-                <button type="button" class="btn btn-outline-primary">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply" viewBox="0 0 16 16">
-                <path d="M6.598 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.74 8.74 0 0 0-1.921-.306 7.404 7.404 0 0 0-.798.008h-.013l-.005.001h-.001L7.3 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L2.614 8.254a.503.503 0 0 0-.042-.028.147.147 0 0 1 0-.252.499.499 0 0 0 .042-.028l3.984-2.933zM7.8 10.386c.068 0 .143.003.223.006.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96v-.667z"/>
-                </svg>
-                  <span class="visually-hidden">Reply</span>
-                </button> 
-                <button type="button" class="btn btn-outline-primary">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive" viewBox="0 0 16 16">
-                    <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"></path>
-                  </svg>
-                  <span class="visually-hidden">Archive</span>
-                </button>                               
-              </div>
-      `;
-      let div = document.createElement('div');
+      email_views.innerHTML = ``;
 
-      // mark as read
-      email_read(id, true);     
-      // email_archived(id, false);
+      let div = document.createElement('div');
+      div.className = 'btn-group mt-2';
+       // create reply button & append to DOMContentLoaded
+       const reply = document.createElement('button');
+       reply.className = "btn btn-outline-primary";       
+       reply.innerHTML = replyIcon;
+       reply.addEventListener('click', function() {
+        console.log('click of reply');
+        compose_email();    
+         // populate fields with information from email
+        document.querySelector('#compose-recipients').value = email['sender'];
+        let subject = email['subject'];
+        console.log(subject.split(" ", 1)[0]);
+        if (subject.split(" ", 1)[0] != "Re:") {
+          subject = "Re: " + subject;
+        }
+        document.querySelector('#compose-subject').value = subject;
+    
+        let body = `
+          On ${email['timestamp']}, ${email['sender']} wrote: ${email['body']}
+        `;
+        document.querySelector('#compose-body').value = body;
+       });
+       div.appendChild(reply);    
+    
+       const grparchive = document.createElement('button');
+       grparchive.className = "btn btn-outline-primary";
+       grparchive.innerHTML = !email['archived'] ? archiveIcon:unArchiveIcon;
+      //  grparchive.addEventListener('click', email_archived(email['id'], !email['archived']));
+       grparchive.addEventListener('click', () => {
+        email_archived(email['id'], !email['archived']);        
+        load_mailbox('archive');
+       });
+       div.appendChild(grparchive);
+    
+       const grpread = document.createElement('button');
+       grpread.className = "btn btn-outline-primary";
+       grpread.innerHTML = !email['read'] ? readIcon:unReadIcon;
+       grpread.addEventListener('click', () => { 
+        email_read(email['id'], !email['read']);
+        load_mailbox('inbox');
+      });
+       div.appendChild(grpread);      
+      // email_views.innerHTML = '';
+      // btnGroup = buttongroup(email);
+      // email_views.appendChild(btnGroup);
+      // Show the mailbox name
+      // email_views.innerHTML += `<h3 class="mt-3">${email['subject'].toUpperCase()}</h3><span class="badge badge-primary">date: ${email['timestamp']}</span>`;
+      let the_email = document.createElement('div');
+      the_email.innerHTML = `
+      <h3 class="mt-3">${email['subject'].toUpperCase()}</h3><span class="badge badge-primary">date: ${email['timestamp']}</span>
+      <span>from: ${email['sender']}</span>
+      <span>to: ${email['recipients']} </span>    
+      <p>${email['body']}</p>`;
+
+      email_views.appendChild(div);
+      email_views.appendChild(the_email);                
   });
 }
 
@@ -98,13 +199,13 @@ function email_read(id, status){
   fetch('/emails/' + id, {
     method: 'PUT',
     body: JSON.stringify({ read : status })
-  })
+  });
 }
 function email_archived(id, status){
   fetch('/emails/' + id, {
     method: 'PUT',
-    body: JSON.stringify({ archived : status })
-  })
+    body: JSON.stringify({ archived : status })    
+  });
 }
 
 function load_mailbox(mailbox) {
@@ -135,8 +236,7 @@ function load_mailbox(mailbox) {
         }else {
           ahref.classList.remove("read");
         }                
-        ahref.innerHTML = `
-            <label><input data-section="${email['id']}" type="checkbox"></label> 
+        ahref.innerHTML = `            
             <span class="badge badge-info m-2">${email['sender']}</span>
             <span class="pr-2">${email['subject']}</span>
             <span class="text-muted text-truncate" style="font-size: 11px; max-width: 25%">${email['body']}</span>
@@ -148,16 +248,6 @@ function load_mailbox(mailbox) {
         div.appendChild(ahref);        
         email_views.appendChild(div);
     });
-    });
-  // <div class="list-group">
-  //           <a href="#" class="list-group-item text-decoration-none read">                
-  //               <label><input type="checkbox"></label>                
-  //               <span class="badge badge-info m-2">Andres posada</span>
-  //               <span class="pr-2">Nice work on the lastest version</span>
-  //               <span class="text-muted text-truncate" style="font-size: 11px; max-width: 25%">- More content here aasdasdasdsadasdsadsadsadsadasdasdsdasdasdasdasasdasdasdasdasdsadasdasd</span>
-  //               <span class="badge badge-primary float-right m-2">12:10 AM</span>                
-  //           </a> 
-  // </div>
-
+    });  
 }
 
