08f73e92f07a8f9964d5a339fbb6c7ddd93251a4
gmarte <giancarlo.marte@gmail.com>
2022-09-20 01:42:13 -0400

    edit / like feat

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -5,15 +5,41 @@ const unlike = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" f
 <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
 </svg>`;      
 
-document.addEventListener('DOMContentLoaded', function(){
-    
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("ini DOM");
+  var spans = document.querySelectorAll('span');    
+  
+  for (var i = 0; i < spans.length; i++) {
+      var self = spans[i];
+      if (self.dataset.id){
+        self.addEventListener('click', function (event) {  
+          // prevent browser's default action
+          event.preventDefault();
+          console.log("muah calling" + this.dataset.id);
+          // calling the like action
+          likeAction(this.dataset.id); // 'this' refers to the current span on for loop
+        }, false);
+      }      
+  }
 });
 
-function like(id){
-    console.log("sending ajax");
-    fetch('/posts/' + id, {
-        method: 'PUT',
-        body: JSON.stringify({ like : True })
-      });    
-    window.location.reload();    
+function likeAction(id){
+  console.log("sending ajax");
+  fetch('/posts/' + id, {
+      method: 'PUT',
+      body: JSON.stringify({ like : 'true' })
+    }).then(response => { 
+      if (!response.ok) {throw response } //throw exception
+      else{return response.json()}
+    }).then(result => {
+      console.log(result); // console log the result
+      if (result.like_post){
+        document.querySelector("#post_like_"+id).innerHTML = like;        
+      }
+      else{
+        document.querySelector("#post_like_"+id).innerHTML = unlike;
+      }
+      document.querySelector("#like_count_"+id).innerHTML = result.likes_count;
+    });    
+  // window.location.reload();    
 }
--- network/views.py
+++ network/views.py
@@ -75,13 +75,19 @@ def profile(request, user_id):
         follower = profile_user.followers.filter(id=user.id).exists()
     else:
         follower = False
+    if request.method == "POST" and user.id != profile_user.id:
+        if follower:
+            user.following.remove(profile_user.id)
+            follower = False
+        else:
+            user.following.add(profile_user.id)
+            follower = True
 
     return render(request, "network/profile.html", {
         "profile": profile_user,
         "follower": follower,
         "posts": Post.objects.filter(creator=profile_user).all()
-    })
-
+    }) 
 
 @csrf_exempt
 @login_required
@@ -96,18 +102,20 @@ def posts(request, post_id=''):
             return render(request, "network/index.html", {
                 "message": "You must complete the description."
             })
+        return HttpResponseRedirect(reverse("index"))
     elif request.method == "PUT":
         try:
             post = Post.objects.get(pk=post_id)
         except Post.DoesNotExist:
             return JsonResponse({"error": "Post not found."}, status=404)
         data = json.loads(request.body)
-        if data.get("likes") is not None:
-            if post.post_likes.filter(user=user).count() > 0:
-                post.likes.remove(user=user)
-            else:
-                post.likes.add(user=user)
-        post.save()
-        return HttpResponse(status=204)
-
-    return HttpResponseRedirect(reverse("index"))
+        if data.get("like") is not None:
+            if (user.post_likes.filter(pk=post_id).exists()):
+                post.likes.remove(user)
+                like_post = False
+            else: # add the like to the post 
+                post.likes.add(user)
+                like_post = True
+        likes_count = post.likes_count
+        return JsonResponse({"likes_count": likes_count, "like_post":like_post}, status=200)
+        # return HttpResponse(status=204)    
